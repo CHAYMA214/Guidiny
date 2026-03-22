33a322455becdfd4290dfb2bd0cf7f3c13534fda
chayma <chaymabaklouti59@gmail.com>
2026-03-22 16:37:09 +0100

    some change

--- backend/util/location.js
+++ backend/util/location.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const HttpError = require('../models/http-error');
 const NOMINATIM_URL = 'https://nominatim.openstreetmap.org/search';
+
 async function getCoordsForAddress(address) {
   const response = await axios.get(NOMINATIM_URL, {
     params: {
@@ -8,23 +9,18 @@ async function getCoordsForAddress(address) {
       format: 'json',
       limit: 1,
     },
+    headers: {
+      'User-Agent': 'Guidiny/1.0 (chaymabaklouti59@gmail.com)'
+    }
   });
 
   const data = response.data;
 
   if (!data || data.length === 0) {
-    const error = new HttpError(
-      'Could not find location for the specified address.',
-      422
-    );
-    throw error;
+    throw new HttpError('Could not find location for the specified address.', 422);
   }
-  const coordinates = {
-    lat: data[0].lat,
-    lng: data[0].lon,
-  };
 
-  return coordinates;
+  return { lat: data[0].lat, lng: data[0].lon };
 }
 
 module.exports = getCoordsForAddress;
